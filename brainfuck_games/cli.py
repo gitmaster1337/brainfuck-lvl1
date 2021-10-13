da52b340bda07211ec28d1f9897a5ef7e01b66e3
gitmaster1337 <gitmaster1337@turbomail.me>
2021-10-13 15:59:46 +0300

    edit cli.py welcome prompt logic

--- brainfuck_games/cli.py
+++ brainfuck_games/cli.py
@@ -5,6 +5,12 @@ import prompt
 
 
 def welcome_user():
-    """Prompt user name, print welcome message."""
+    """Prompt user name, print welcome message.
+
+    Returns:
+        name: Name entered by user
+    """
+    print('Welcome to Brainfuck Games!')
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(name))
+    return name
